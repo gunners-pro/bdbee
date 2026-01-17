c96b35307f42a48db822b8e831c576d27a8a2e34
Fabricyo Barreto <bass.barreto@gmail.com>
2026-01-16 23:18:13 -0300

    feat: add simple command line

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::{
     error::DBError,
     pager::{OpenMode, Pager},
 };
+use std::env;
 
 mod error;
 mod pager;
@@ -9,9 +10,17 @@ mod pager;
 static PAGE_SIZE: u64 = 4096;
 
 fn main() -> Result<(), DBError> {
-    println!("Iniciando bdbee...");
-    let _pager =
-        Pager::open("data.db", PAGE_SIZE, OpenMode::Create).expect("Falha ao iniciar banco");
-    println!("Banco iniciado com sucesso");
+    let args: Vec<String> = env::args().collect();
+
+    let mode = match args.get(1).map(|s| s.as_str()) {
+        Some("create") => OpenMode::Create,
+        Some("open") => OpenMode::Open,
+        _ => {
+            eprintln!("Uso: bdbee [create|open]");
+            std::process::exit(1);
+        }
+    };
+
+    Pager::open("data.db", PAGE_SIZE, mode).expect("Falha ao iniciar banco");
     Ok(())
 }
